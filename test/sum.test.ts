008617b1e4f2b65c2a838ec314633403c1cbf103
chuyuan du <chuyuandu@163.com>
2025-04-30 14:25:18 +0800

    格式优化
    * 格式优化

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,27 +1,31 @@
-import { describe, expect, it } from "vitest";
-import { handleArgs } from "../src/index";
-import pkg from '../package.json'
-import { helpContent } from "../src/util";
+import { describe, expect, it } from 'vitest';
+import { handleArgs } from '../src/index';
+import pkg from '../package.json';
+import { helpContent } from '../src/util';
 
-describe("show version", () => {
-  it("显示版本号", () => {
-    expect(handleArgs({
-      '_': [],
-      '--version': true,
-      tgzFolder: '_test_',
-      cwd: '_virtual_',
-    })).toEqual(pkg.version);
+describe('show version', () => {
+  it('显示版本号', () => {
+    expect(
+      handleArgs({
+        _: [],
+        '--version': true,
+        tgzFolder: '_test_',
+        cwd: '_virtual_',
+      }),
+    ).toEqual(pkg.version);
   });
 });
 
-describe("show help", () => {
-  it("显示帮助信息", () => {
-    expect(handleArgs({
-      '_': [],
-      '--help': true,
-      tgzFolder: '_test_',
-      cwd: '_virtual_',
-    })).toEqual(helpContent);
+describe('show help', () => {
+  it('显示帮助信息', () => {
+    expect(
+      handleArgs({
+        _: [],
+        '--help': true,
+        tgzFolder: '_test_',
+        cwd: '_virtual_',
+      }),
+    ).toEqual(helpContent);
   });
 });
 
